e62d35d3076c6aed2ea319d4cefe4ed7a71c6ba8
KhadraouiAmani <amani.khadraoui@insat.ucar.tn>
2026-03-23 18:24:51 +0100

    Initialisation du projet BiblioTech - Version JPA par Amani

---
+++ src/main/java/com/example/tp2jeepart2/Tp2jeePart2Application.java
@@ -0,0 +1,13 @@
+package com.example.tp2jeepart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2jeePart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp2jeePart2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp2jeepart2/Tp2jeePart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp2jeepart2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2jeePart2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
